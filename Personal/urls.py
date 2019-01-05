8c4eeb0a90a54beb0c27e7c65188a22070d1e6a5
ChekeGT <chekelosos@gmail.com>
2019-01-05 16:26:07 -0600

    Inicializada la app del Blog

--- Personal/urls.py
+++ Personal/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^portafolio/', include('portfolio.urls')),
-    url(r'', include('core.urls'))
-
+    url(r'', include('core.urls')),
+    url(r'', include('blog.urls'))
 ]
